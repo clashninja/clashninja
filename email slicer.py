bf0c08914cdb67b47f65403e430977937abc9489
clashninja <89434947+clashninja@users.noreply.github.com>
2021-08-24 08:55:16 +0530

    Update email slicer.py

--- email slicer.py
+++ email slicer.py
@@ -1,1 +1,18 @@
+#getting user email address
+email=input("Enter your email address: ").strip()
+
+#slice out username
+username=email[:email.index("@")]
+
+#slice out domain
+doamin=email[email.index(@)+1:]
+
+#output format message
+output=print("Your email address is",email,/
+             "username is",username,/
+             "domain is",domain)
+
+#final output
+print(output)
+
 
